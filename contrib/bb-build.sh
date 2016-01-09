5fb204d926a8aed881fa01e3cee137977721f56b
Anton Skshidlevsky <meefik@gmail.com>
2016-01-10 00:59:49 +0300

    Fixed DNS resolver for static builds

--- contrib/bb-build.sh
+++ contrib/bb-build.sh
@@ -7,16 +7,41 @@
 # Make a patch:
 # diff -urN ../busybox-${BB_VERSION}.orig/ . > ../patches-${BB_VERSION}/${PATCH_NAME}.patch
 
+helper()
+{
+    echo "Usage: $0 <arm|intel|mips> <pie|nopie|static>"
+    exit 1
+}
+
 BB_VERSION="1.24.1"
 ANDROID_NATIVE_API_LEVEL="android-9"
 MARCH="$1"
-PIE="$2"
-[ -z "$MARCH" ] && { echo "Usage: $0 <arm|intel|mips> [pie]"; exit 1; }
-[ -z "$ANDROID_NDK_ROOT" ] && ANDROID_NDK_ROOT="$HOME/Android/Sdk/ndk-bundle"
+PARAM="$2"
 HOST_ARCH=$(uname -m)
 NCPU=$(grep -ci processor /proc/cpuinfo)
 PREFIX="../compiled/$MARCH"
-[ -n "$PIE" ] && PREFIX="$PREFIX/pie" || PREFIX="$PREFIX/nopie"
+[ -z "$ANDROID_NDK_ROOT" ] && ANDROID_NDK_ROOT="$HOME/Android/Sdk/ndk-bundle"
+case "$MARCH" in
+arm|intel|mips)
+;;
+*)
+    helper
+;;
+esac
+case "$PARAM" in
+pie)
+    PREFIX="$PREFIX/pie"
+;;
+nopie)
+    PREFIX="$PREFIX/nopie"
+;;
+static)
+    PREFIX="$PREFIX/static"
+;;
+*)
+    helper
+;;
+esac
 
 pkg="busybox-$BB_VERSION"
 defconfig="android_ndk_defconfig"
@@ -32,32 +57,32 @@ echo ">>> patch"
 cd $pkg
 for p in $(ls ../patches-$BB_VERSION/*.patch)
 do
-	patch -b -p0 < $p || exit 1
+    patch -b -p0 < $p || exit 1
 done
 cp ../patches-$BB_VERSION/$defconfig ./configs/$defconfig
 
 echo ">>> config"
 case "$MARCH" in
 arm)
-	CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-$HOST_ARCH/bin/arm-linux-androideabi-"
-	CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-arm"
-	CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -nostdlib -march=armv5te -msoft-float -mfloat-abi=softfp -mfpu=neon -mthumb -mthumb-interwork -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
-	CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-	CONFIG_EXTRA_LDLIBS="m c gcc"
+    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-$HOST_ARCH/bin/arm-linux-androideabi-"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-arm"
+    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=armv5te -msoft-float -mfloat-abi=softfp -mfpu=neon -mthumb -mthumb-interwork -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
+    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
+    CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 intel)
-	CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/x86-4.9/prebuilt/linux-$HOST_ARCH/bin/i686-linux-android-"
-	CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-x86"
-	CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -nostdlib -march=i686 -mtune=atom -fpic $CFLAGS"
-	CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-	CONFIG_EXTRA_LDLIBS="m c gcc"
+    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/x86-4.9/prebuilt/linux-$HOST_ARCH/bin/i686-linux-android-"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-x86"
+    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=i686 -mtune=atom -fpic $CFLAGS"
+    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
+    CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 mips)
-	CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/mipsel-linux-android-4.9/prebuilt/linux-$HOST_ARCH/bin/mipsel-linux-android-"
-	CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-mips"
-	CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -nostdlib -march=mips32 -fpic -Wno-psabi -fomit-frame-pointer -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers $CFLAGS"
-	CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-	CONFIG_EXTRA_LDLIBS="m c gcc"
+    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/mipsel-linux-android-4.9/prebuilt/linux-$HOST_ARCH/bin/mipsel-linux-android-"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-mips"
+    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=mips32 -fpic -Wno-psabi -fomit-frame-pointer -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers $CFLAGS"
+    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
+    CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 esac
 sed -i "s|^CONFIG_CROSS_COMPILER_PREFIX=.*|CONFIG_CROSS_COMPILER_PREFIX=\"$CONFIG_CROSS_COMPILER_PREFIX\"|" ./configs/$defconfig
@@ -65,10 +90,20 @@ sed -i "s|^CONFIG_SYSROOT=.*|CONFIG_SYSROOT=\"$CONFIG_SYSROOT\"|" ./configs/$def
 sed -i "s|^CONFIG_EXTRA_CFLAGS=.*|CONFIG_EXTRA_CFLAGS=\"$CONFIG_EXTRA_CFLAGS\"|" ./configs/$defconfig
 sed -i "s|^CONFIG_EXTRA_LDFLAGS=.*|CONFIG_EXTRA_LDFLAGS=\"$CONFIG_EXTRA_LDFLAGS\"|" ./configs/$defconfig
 sed -i "s|^CONFIG_EXTRA_LDLIBS=.*|CONFIG_EXTRA_LDLIBS=\"$CONFIG_EXTRA_LDLIBS\"|" ./configs/$defconfig
-if [ -n "$PIE" ]
-then sed -i "s|^# CONFIG_PIE.*|CONFIG_PIE=y|" ./configs/$defconfig
-else sed -i "s|^CONFIG_PIE.*|# CONFIG_PIE is not set|" ./configs/$defconfig
-fi
+case "$PARAM" in
+pie)
+    sed -i "s|^CONFIG_STATIC.*|# CONFIG_STATIC is not set|" ./configs/$defconfig
+    sed -i "s|^# CONFIG_PIE.*|CONFIG_PIE=y|" ./configs/$defconfig
+;;
+nopie)
+    sed -i "s|^CONFIG_STATIC.*|# CONFIG_STATIC is not set|" ./configs/$defconfig
+    sed -i "s|^CONFIG_PIE.*|# CONFIG_PIE is not set|" ./configs/$defconfig
+;;
+static)
+    sed -i "s|^# CONFIG_STATIC.*|CONFIG_STATIC=y|" ./configs/$defconfig
+    sed -i "s|^CONFIG_PIE.*|# CONFIG_PIE is not set|" ./configs/$defconfig
+;;
+esac
 sed -i "s|^EXTRAVERSION =.*|EXTRAVERSION = -meefik|" ./Makefile
 make $defconfig || exit 1
 
