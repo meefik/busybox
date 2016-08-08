8981c440d0f98207c298ddc4135398e0ab36dde7
Anton Skshidlevsky <meefik@gmail.com>
2016-08-08 16:33:55 +0300

    Updated BusyBox build tool

--- contrib/bb-build.sh
+++ contrib/bb-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# BusyBox build tools
-# (C) 2014-2015 Anton Skshidlevsky <meefik@gmail.com>, GPLv3
+# BusyBox build tool
+# (C) 2014-2016 Anton Skshidlevsky <meefik@gmail.com>, GPLv3
 # Requires:
 # Android NDK r10d (http://developer.android.com/ndk/downloads/index.html)
 # export ANDROID_NDK_ROOT="/path/to/ndk"
@@ -14,18 +14,18 @@ helper()
 }
 
 BB_VERSION="1.24.2"
-ANDROID_NATIVE_API_LEVEL="android-21"
+ANDROID_NATIVE_API_LEVEL="9"
 GCC_VERSION="4.9"
 MARCH="$1"
-PARAM="$2"
+LINKER="$2"
 NCPU=$(grep -ci processor /proc/cpuinfo)
 PREFIX="../compiled/$MARCH"
 [ -z "$ANDROID_NDK_ROOT" ] && ANDROID_NDK_ROOT="$HOME/Android/Sdk/ndk-bundle"
-[ "$MARCH" == "arm64" ] && ANDROID_NATIVE_API_LEVEL="android-21"
+[ "$LINKER" = "static" -o "$MARCH" = "arm64" ] && ANDROID_NATIVE_API_LEVEL="21"
 # Extra config required for specific API levels
 # This will be prepended to the .config before building
 EXTRACONFIG=""
-[ ${ANDROID_NATIVE_API_LEVEL#android-} -ge 21 ] && EXTRACONFIG="android_ndk_defconfig-sdk21"
+[ "$ANDROID_NATIVE_API_LEVEL" -ge 21 ] && EXTRACONFIG="android_ndk_defconfig-sdk21"
 
 case "$MARCH" in
 arm|arm64|intel|mips)
@@ -35,7 +35,7 @@ arm|arm64|intel|mips)
 ;;
 esac
 
-case "$PARAM" in
+case "$LINKER" in
 pie)
     PREFIX="$PREFIX/pie"
 ;;
@@ -85,28 +85,28 @@ echo ">>> config"
 case "$MARCH" in
 arm)
     CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/arm-linux-androideabi-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-arm"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-arm"
     CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=armv5te -msoft-float -mfloat-abi=softfp -mfpu=neon -mthumb -mthumb-interwork -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
     CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
     CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 arm64)
     CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/aarch64-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-arm64"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-arm64"
     CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=armv8-a -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
     CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
     CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 intel)
     CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/x86-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/i686-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-x86"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-x86"
     CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=i686 -mtune=atom -fpic $CFLAGS"
     CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
     CONFIG_EXTRA_LDLIBS="m c gcc"
 ;;
 mips)
     CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/mipsel-linux-android-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/mipsel-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_NATIVE_API_LEVEL/arch-mips"
+    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-mips"
     CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=mips32 -fpic -Wno-psabi -fomit-frame-pointer -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers $CFLAGS"
     CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
     CONFIG_EXTRA_LDLIBS="m c gcc"
@@ -117,7 +117,7 @@ sed -i "s|^CONFIG_SYSROOT=.*|CONFIG_SYSROOT=\"$CONFIG_SYSROOT\"|" ./configs/$def
 sed -i "s|^CONFIG_EXTRA_CFLAGS=.*|CONFIG_EXTRA_CFLAGS=\"$CONFIG_EXTRA_CFLAGS\"|" ./configs/$defconfig
 sed -i "s|^CONFIG_EXTRA_LDFLAGS=.*|CONFIG_EXTRA_LDFLAGS=\"$CONFIG_EXTRA_LDFLAGS\"|" ./configs/$defconfig
 sed -i "s|^CONFIG_EXTRA_LDLIBS=.*|CONFIG_EXTRA_LDLIBS=\"$CONFIG_EXTRA_LDLIBS\"|" ./configs/$defconfig
-case "$PARAM" in
+case "$LINKER" in
 pie)
     sed -i "s|^CONFIG_STATIC.*|# CONFIG_STATIC is not set|" ./configs/$defconfig
     sed -i "s|^# CONFIG_PIE.*|CONFIG_PIE=y|" ./configs/$defconfig
