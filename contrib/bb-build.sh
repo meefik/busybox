d6fdc3669b566d8a893fe32b381542632684899e
meefik <meefik@gmail.com>
2019-11-08 13:53:48 +0300

    Fixed and added x86_64 build; Fixed problem with loop mount; Updated bb-build script

--- contrib/bb-build.sh
+++ contrib/bb-build.sh
@@ -7,142 +7,81 @@
 # export ANDROID_NDK_ROOT="/path/to/ndk"
 #
 # Make a patch:
-# diff -urN ../busybox-${BB_VERSION}.orig/ . > ../patches-${BB_VERSION}/${PATCH_NAME}.patch
+# diff -urN ../busybox-${BB_VERSION}.orig/ . > ../patches/${BB_VERSION}/${PATCH_NAME}.patch
 
-helper()
-{
-    echo "Usage: $0 <arm|arm64|x86|x86_64|mips|mips64> <pie|nopie|static>"
-    exit 1
-}
+set -e -x
 
 BB_VERSION="1.31.1"
 ANDROID_NATIVE_API_LEVEL="21"
-GCC_VERSION="4.9"
 MARCH="$1"
 LINKER="$2"
-NCPU=$(grep -ci processor /proc/cpuinfo)
-PREFIX="../compiled/$MARCH"
+NCPU="$(grep -ci processor /proc/cpuinfo)"
+SOURCE_DIR="$(pwd)"
+BUILD_DIR="$SOURCE_DIR/build/$MARCH"
+INSTALL_DIR="$BUILD_DIR/dist"
 [ -z "$ANDROID_NDK_ROOT" ] && ANDROID_NDK_ROOT="$HOME/android-ndk-r15c"
-[ "$LINKER" = "static" -o "$MARCH" = "arm64" ] && ANDROID_NATIVE_API_LEVEL="21"
+SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-$MARCH"
 
 case "$MARCH" in
-arm|arm64|x86|x86_64|mips|mips64)
+arm)
+  TARGET_HOST="arm-linux-androideabi"
+  CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -D__POSIX_VISIBLE=199209 -D__BSD_VISIBLE -nostdlib -march=armv5te -msoft-float -mfloat-abi=softfp -mthumb -fPIC -I$SYSROOT/usr/include/$TARGET_HOST -I$SYSROOT/usr/include"
+  LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined -static -L$SYSROOT/usr/lib"
+  LDLIBS="m c gcc"
 ;;
-*)
-    helper
+arm64)
+  TARGET_HOST="aarch64-linux-android"
+  CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -D__POSIX_VISIBLE=199209 -D__BSD_VISIBLE -nostdlib -march=armv8-a -fno-short-enums -fgcse-after-reload -frename-registers -fPIC -I$SYSROOT/usr/include/$TARGET_HOST -I$SYSROOT/usr/include"
+  LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined -static -L$SYSROOT/usr/lib"
+  LDLIBS="m c gcc"
 ;;
-esac
-
-case "$LINKER" in
-pie)
-    PREFIX="$PREFIX/pie"
-;;
-nopie)
-    PREFIX="$PREFIX/nopie"
-;;
-static)
-    PREFIX="$PREFIX/static"
-;;
-*)
-    helper
-;;
-esac
-
-case "$(uname -m)"  in
-i[3-6]86)
-    HOST_ARCH="x86"
+x86)
+  TARGET_HOST="i686-linux-android"
+  CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -D__POSIX_VISIBLE=199209 -D__BSD_VISIBLE -nostdlib -march=i686 -mtune=atom -fPIC -I$SYSROOT/usr/include/$TARGET_HOST -I$SYSROOT/usr/include"
+  LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined -static -L$SYSROOT/usr/lib"
+  LDLIBS="m c gcc"
 ;;
 x86_64)
-    HOST_ARCH="x86_64"
+  TARGET_HOST="x86_64-linux-android"
+  CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -D__POSIX_VISIBLE=199209 -D__BSD_VISIBLE -nostdlib -march=x86-64 -mtune=atom -fPIC -I$SYSROOT/usr/include/$TARGET_HOST -I$SYSROOT/usr/include"
+  LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined -static -L$SYSROOT/usr/lib"
+  LDLIBS="m c gcc"
 ;;
 *)
-    echo "Unknown architecture"
-    exit 1
+  echo "Usage: $0 <arm|arm64|x86|x86_64>"
+  exit 1
 ;;
 esac
+
+PATH="$ANDROID_NDK_ROOT/toolchains/$TARGET_HOST-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
+
+[ -e "$BUILD_DIR" ] || mkdir -p "$BUILD_DIR";
+[ -e "$INSTALL_DIR" ] || mkdir -p "$INSTALL_DIR";
 
 pkg="busybox-$BB_VERSION"
 defconfig="android_ndk_defconfig"
 
 echo ">>> download"
-[ -e "$pkg.tar.bz2" ] || wget --progress=dot http://busybox.net/downloads/$pkg.tar.bz2 || exit 1
+[ -e "$BUILD_DIR/$pkg.tar.bz2" ] || wget http://busybox.net/downloads/$pkg.tar.bz2 -O "$BUILD_DIR/$pkg.tar.bz2" || exit 1
 
 echo ">>> unpack"
-[ -d "$pkg" ] && rm -rf $pkg
-tar jvxf $pkg.tar.bz2 || exit 1
+[ -d "$BUILD_DIR/$pkg" ] && rm -rf "$BUILD_DIR/$pkg"
+tar jxf "$BUILD_DIR/$pkg.tar.bz2" -C "$BUILD_DIR" || exit 1
 
 echo ">>> patch"
-cd $pkg
-for p in $(ls ../patches-$BB_VERSION/*.patch)
+cd "$BUILD_DIR/$pkg"
+for p in $(ls $SOURCE_DIR/patches/$BB_VERSION/*.patch)
 do
     patch -b -p0 < $p || exit 1
 done
-cp ../patches-$BB_VERSION/$defconfig ./configs/$defconfig
+cp $SOURCE_DIR/patches/$BB_VERSION/$defconfig ./configs/$defconfig
 
 echo ">>> config"
-case "$MARCH" in
-arm)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/arm-linux-androideabi-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-arm"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=armv5te -msoft-float -mfloat-abi=softfp -mthumb -mthumb-interwork -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-arm64)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/aarch64-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-arm64"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=armv8-a -fpic -fno-short-enums -fgcse-after-reload -frename-registers $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-x86)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/x86-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/i686-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-x86"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=i686 -mtune=atom -fpic $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-x86_64)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/x86_64-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/x86_64-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-x86_64"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=x86-64 -mtune=atom -fpic $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib64/crtbegin_dynamic.o \${SYSROOT}/usr/lib64/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-mips)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/mipsel-linux-android-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/mipsel-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-mips"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=mips32 -fpic -Wno-psabi -fomit-frame-pointer -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib/crtbegin_dynamic.o \${SYSROOT}/usr/lib/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-mips64)
-    CONFIG_CROSS_COMPILER_PREFIX="$ANDROID_NDK_ROOT/toolchains/mips64el-linux-android-$GCC_VERSION/prebuilt/linux-$HOST_ARCH/bin/mips64el-linux-android-"
-    CONFIG_SYSROOT="$ANDROID_NDK_ROOT/platforms/android-$ANDROID_NATIVE_API_LEVEL/arch-mips64"
-    CONFIG_EXTRA_CFLAGS="-DANDROID -D__ANDROID__ -DSK_RELEASE -nostdlib -march=mips64r6 -fpic -Wno-psabi -fomit-frame-pointer -fno-strict-aliasing -finline-functions -ffunction-sections -funwind-tables -fmessage-length=0 -fno-inline-functions-called-once -fgcse-after-reload -frerun-cse-after-loop -frename-registers $CFLAGS"
-    CONFIG_EXTRA_LDFLAGS="-Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -dynamic-linker -Xlinker /system/bin/linker64 -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined \${SYSROOT}/usr/lib64/crtbegin_dynamic.o \${SYSROOT}/usr/lib64/crtend_android.o -fuse-ld=bfd $LDFLAGS"
-    CONFIG_EXTRA_LDLIBS="m c gcc"
-;;
-esac
-sed -i "s|^CONFIG_CROSS_COMPILER_PREFIX=.*|CONFIG_CROSS_COMPILER_PREFIX=\"$CONFIG_CROSS_COMPILER_PREFIX\"|" ./configs/$defconfig
-sed -i "s|^CONFIG_SYSROOT=.*|CONFIG_SYSROOT=\"$CONFIG_SYSROOT\"|" ./configs/$defconfig
-sed -i "s|^CONFIG_EXTRA_CFLAGS=.*|CONFIG_EXTRA_CFLAGS=\"$CONFIG_EXTRA_CFLAGS\"|" ./configs/$defconfig
-sed -i "s|^CONFIG_EXTRA_LDFLAGS=.*|CONFIG_EXTRA_LDFLAGS=\"$CONFIG_EXTRA_LDFLAGS\"|" ./configs/$defconfig
-sed -i "s|^CONFIG_EXTRA_LDLIBS=.*|CONFIG_EXTRA_LDLIBS=\"$CONFIG_EXTRA_LDLIBS\"|" ./configs/$defconfig
-case "$LINKER" in
-pie)
-    sed -i "s|^CONFIG_STATIC.*|# CONFIG_STATIC is not set|" ./configs/$defconfig
-    sed -i "s|^# CONFIG_PIE.*|CONFIG_PIE=y|" ./configs/$defconfig
-;;
-nopie)
-    sed -i "s|^CONFIG_STATIC.*|# CONFIG_STATIC is not set|" ./configs/$defconfig
-    sed -i "s|^CONFIG_PIE.*|# CONFIG_PIE is not set|" ./configs/$defconfig
-;;
-static)
-    sed -i "s|^# CONFIG_STATIC.*|CONFIG_STATIC=y|" ./configs/$defconfig
-    sed -i "s|^CONFIG_PIE.*|# CONFIG_PIE is not set|" ./configs/$defconfig
-;;
-esac
+sed -i "s|^CONFIG_CROSS_COMPILER_PREFIX=.*|CONFIG_CROSS_COMPILER_PREFIX=\"$TARGET_HOST-\"|" ./configs/$defconfig
+sed -i "s|^CONFIG_SYSROOT=.*|CONFIG_SYSROOT=\"$SYSROOT\"|" ./configs/$defconfig
+sed -i "s|^CONFIG_EXTRA_CFLAGS=.*|CONFIG_EXTRA_CFLAGS=\"$CFLAGS\"|" ./configs/$defconfig
+sed -i "s|^CONFIG_EXTRA_LDFLAGS=.*|CONFIG_EXTRA_LDFLAGS=\"$LDFLAGS\"|" ./configs/$defconfig
+sed -i "s|^CONFIG_EXTRA_LDLIBS=.*|CONFIG_EXTRA_LDLIBS=\"$LDLIBS\"|" ./configs/$defconfig
 sed -i "s|^EXTRAVERSION =.*|EXTRAVERSION = -meefik|" ./Makefile
 make $defconfig || exit 1
 
@@ -151,4 +90,27 @@ unset CFLAGS LDFLAGS
 make -j$NCPU || exit 1
 
 echo ">>> install"
-make CONFIG_PREFIX=$PREFIX install || exit 1
+make CONFIG_PREFIX="$INSTALL_DIR" install || exit 1
+
+echo ">>> ssl_helper"
+# export AR=llvm-ar
+# export AS=llvm-as
+# export CC=clang
+# export CXX=clang++
+# export LD=$TARGET_HOST-ld
+# export STRIP=$TARGET_HOST-strip
+
+if [ ! -e "$BUILD_DIR/v3.13.0-stable.tar.gz" ]
+then
+  wget https://github.com/wolfSSL/wolfssl/archive/v3.13.0-stable.tar.gz -O "$BUILD_DIR/v3.13.0-stable.tar.gz"
+fi
+[ -e "$BUILD_DIR/wolfssl-3.13.0-stable" ] && rm -r "$BUILD_DIR/wolfssl-3.13.0-stable"
+tar xzf "$BUILD_DIR/v3.13.0-stable.tar.gz" -C "$BUILD_DIR"
+cd "$BUILD_DIR/wolfssl-3.13.0-stable"
+./autogen.sh
+./configure --enable-static --enable-singlethreaded --disable-shared --host=$TARGET_HOST
+make -j$NCPU
+
+#$CC $CFLAGS $LDFLAGS -Os -Wall -I "$BUILD_DIR/wolfssl-3.13.0-stable" -c "$SOURCE_DIR/ssl_helper.c" -o "$BUILD_DIR/ssl_helper.o"
+#$CC $CFLAGS $LDFLAGS -static "$BUILD_DIR/ssl_helper.o" "$BUILD_DIR/wolfssl-3.13.0-stable/src/.libs/libwolfssl.a" -lm -o "$INSTALL_DIR/ssl_helper"
+#$STRIP -s "$INSTALL_DIR/ssl_helper"
